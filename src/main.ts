8a03137be7bf825d52b789a6570432b00fa095c7
Diego Chavez <diegochavezcarro@gmail.com>
2024-06-20 13:54:46 -0300

    cors updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: ['http://localhost:5173','http://localhost:4173'], // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
